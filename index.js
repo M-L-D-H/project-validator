96ac435236df7c6589c47357a677bf659a39de1e
Jonas Müller-Laackman <jonas.mueller-laackman@fu-berlin.de>
2022-02-14 17:24:35 +0100

    validation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   const folder = core.getInput('input_folder');
   console.log(`Input folder set to: ${folder}`);
+  const data = fs.readdirSync(folder);
+  console.log('Content', data);
+
 } catch (error) {
   core.setFailed(error.message);
 }
