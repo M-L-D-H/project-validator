7edf02631b71259875546599bf6c6cdc0c82e8b8
Jonas Müller-Laackman <jonas.mueller-laackman@fu-berlin.de>
2022-02-15 15:34:36 +0100

    validation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const Ajv = require('ajv/dist/2020');
+const Ajv2020 = require('ajv/dist/2020');
 const fs = require('fs');
 const path = require('path');
 
