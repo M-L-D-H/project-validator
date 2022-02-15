c170b1173afb17d0db06d1be8683623708d2e3f3
Jonas Müller-Laackman <jonas.mueller-laackman@fu-berlin.de>
2022-02-15 13:14:11 +0100

    validation

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const Ajv = require('ajv');
 const fs = require('fs');
+const path = require('path');
 
 try {
+  const ajv = new Ajv();
   const folder = core.getInput('input_folder');
-  console.log(`Input folder set to: ${folder}`);
-  const data = fs.readdirSync(folder);
-  console.log('Content', data);
+  const schema_path = core.getInput('schema');
 
+  const schema_file = fs.readFileSync(schema_path, {
+    encoding: 'utf-8',
+    flat: 'r'
+  });
+  const schema = ajv.compile(schema_file);
+
+  const data = fs.readdirSync(folder);
+  data.map((dirname) => {
+    const dir_files = fs.readdirSync(dirname);
+    let target = '';
+    dir_files.forEach((file) => {
+      if (path.extname(file) === '.json') {
+        target = fs.readFileSync(file);
+        const valid = validate(target);
+
+        if (!valid) {
+          console.log(validate.errors);
+          core.setFailed(validate.errors);
+        } else {
+          core.setOutput('Everything fine!');
+        }
+      }
+    });
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
